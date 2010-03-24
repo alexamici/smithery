57e9e23cab8b678a929fc881e3b590f51171abfa
Alessandro Amici <a.amici@bopen.it>
2010-03-24 01:12:14 +0100

    new: wrap buildout in a way not to be self destructive (the app.py simulates smithery-wrap.cfg)

--- smithery/app.py
+++ smithery/app.py
@@ -11,8 +11,15 @@ class Smithery(Buildout):
         # in order to change the Buildout class defaults
         # without being too invasive
         options = [
-            ('buildout', 'parts', 'smithery'), 
-            ('smithery', 'recipe', 'smithery.worker'), 
+            # disable bootstrap directories and installed parts tracking
+            ('buildout', 'installed', ''), 
+            ('buildout', 'bin-directory', '.'), 
+            ('buildout', 'eggs-directory', '.'), 
+            ('buildout', 'develop-eggs-directory', '.'), 
+            ('buildout', 'parts-directory', '.'), 
+            # custom default config
+            ('buildout', 'parts', 'smithery'),
+            ('smithery', 'recipe', 'smithery'),
         ] + options
         Buildout.__init__(self, config_file, options, **keys)
 
