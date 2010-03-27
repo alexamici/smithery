1ed644d8e4862967c42aef26b7e92aaaa0fcd2cf
Alessandro Amici <a.amici@bopen.it>
2010-03-27 01:43:37 +0100

    new: minimal cheetah template forge

--- smithery/forge.py
+++ smithery/forge.py
@@ -4,5 +4,10 @@ from pprint import pprint
 from Cheetah.Template import Template
 
 
+def cheetah(namespace, templatepath, outpath=None):
+    template = Template.compile(file=templatepath)
+    with file(outpath or namespace['outpath'], 'w') as outfile:
+        outfile.write(str(template()))
+
 def display(namespace, *args):
     pprint(namespace)
