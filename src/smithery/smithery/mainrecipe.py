f4063505035bed2691133b92dce0d76950ad6564
Alessandro Amici <a.amici@bopen.it>
2010-04-03 15:20:05 +0200

    new: initialize the namespace if needed

--- src/smithery/smithery/mainrecipe.py
+++ src/smithery/smithery/mainrecipe.py
@@ -5,6 +5,10 @@ import sys
 class Worker(object):
     def __init__(self, buildout, name, options):
         self.buildout, self.name, self.options = buildout, name, options
+        try:
+            self.buildout.namespace
+        except AttributeError:
+            self.buildout.namespace = {}
 
     def install(self):
         return ()
