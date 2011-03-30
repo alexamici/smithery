9b73f14c09489c1300cff7a10ecc4fbd01967772
Alessandro Amici <a.amici@bopen.it>
2011-03-30 20:14:50 +0200

    new: add a simple namespace manipulation methods

--- src/smithery.recipe.miners/smithery/recipe/miners/base.py
+++ src/smithery.recipe.miners/smithery/recipe/miners/base.py
@@ -7,3 +7,12 @@ class Miner(object):
             self.buildout.namespace
         except AttributeError:
             self.buildout.namespace = {}
+
+    def set_names(self, names, value):
+        next = self.buildout.namespace
+        for name in names:
+            current = next
+            if name not in current:
+                current[name] = {}
+            next = current[name]
+        current[names[-1]] = value
